5030ee619b96026b0bc0c64fe8d5cce175485ca7
JPKingtero <JPQM4897@gmail.com>
2023-06-28 12:49:56 -0500

    fix(baseTest): redundancy was removed

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -17,29 +17,29 @@ public class BaseTest {
     public WebDriver driver;
     protected HomePage homePage;
 
-    @BeforeTest
-    public void setUp(){
-       // Scanner scanner = new Scanner(System.in);
-       // String choice = scanner.nextLine();
-       // switch(choice){
-       //     case "chrome","Chrome":
-       //         WebDriverManager.chromedriver().setup();
-       //         driver = new ChromeDriver();
-       //         break;
-       //     case "edge","Edge":
-       //         WebDriverManager.edgedriver()
-       // }
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions chromeOptions = new ChromeOptions();
-       // chromeOptions.addArguments("--disable-features=AutomationControlled");
-       // chromeOptions.addExtensions(new File("C:\\Users\\User\\IdeaProjects\\Travelocity_Website_Test\\uBlock-Origin.crx"));
-       // chromeOptions.setAcceptInsecureCerts(true);
-       // chromeOptions.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
-        driver = new ChromeDriver(chromeOptions);
-        driver.manage().window().maximize();
-        driver.get("https://www.despegar.com.co"); //here goes the website URL
-        homePage = new HomePage(driver);
-    }
+  // @BeforeTest
+  // public void setUp(){
+  //    // Scanner scanner = new Scanner(System.in);
+  //    // String choice = scanner.nextLine();
+  //    // switch(choice){
+  //    //     case "chrome","Chrome":
+  //    //         WebDriverManager.chromedriver().setup();
+  //    //         driver = new ChromeDriver();
+  //    //         break;
+  //    //     case "edge","Edge":
+  //    //         WebDriverManager.edgedriver()
+  //    // }
+  //     WebDriverManager.chromedriver().setup();
+  //     ChromeOptions chromeOptions = new ChromeOptions();
+  //    // chromeOptions.addArguments("--disable-features=AutomationControlled");
+  //    // chromeOptions.addExtensions(new File("C:\\Users\\User\\IdeaProjects\\Travelocity_Website_Test\\uBlock-Origin.crx"));
+  //    // chromeOptions.setAcceptInsecureCerts(true);
+  //    // chromeOptions.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+  //     driver = new ChromeDriver(chromeOptions);
+  //     driver.manage().window().maximize();
+  //     driver.get("https://www.despegar.com.co"); //here goes the website URL
+  //     homePage = new HomePage(driver);
+  // }
 
    // @AfterTest
    // public void tearDown(){
